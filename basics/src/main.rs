99ab0fdae08990663a3ba1052a5388be0ea59094
fazlurnu <fazlur.rahman.ae@gmail.com>
2022-10-15 16:34:32 +0200

    add more knowdledge

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,6 +1,8 @@
 mod var_and_mut;
 
 fn main() {
-    var_and_mut::immutable();
-    var_and_mut::mutable();
+    // var_and_mut::immutable();
+    // var_and_mut::mutable();
+    // var_and_mut::constant();
+    var_and_mut::shadowing();
 }
--- basics/src/var_and_mut.rs
+++ basics/src/var_and_mut.rs
@@ -1,3 +1,7 @@
+fn type_of<T>(_: T) -> &'static str {
+    std::any::type_name::<T>()
+}
+
 pub fn immutable(){
     let x = 5;
     println!("Variable x with value {x} is by default immutable");
@@ -8,4 +12,32 @@ pub fn mutable(){
     println!("The init value of x is {x}, we define it as immutable by adding 'mut'");
     x = 10;
     println!("Now the value of x is {x}");
+}
+
+pub fn constant(){
+    const ONE_HOUR_IN_SECONDS: u32 = 1 * 60 * 60;
+    println!("We use const for hardcoded value such as ONE_HOUR_IN_SECONDS: {}", ONE_HOUR_IN_SECONDS);
+    println!("To use const, developer should explicitly define its datatype");
+}
+
+pub fn shadowing(){
+    println!("Here is shadowing");
+    println!("Shadowing spares us from having to come up with different names");
+
+    let x = 5;
+    
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in this scope is {x}");
+    }
+
+    println!("The value of x in this scope is {x}");
+
+    let spaces = "  ";
+    println!("Here, my spaces type is {}", type_of(spaces));
+    let spaces = spaces.len();
+    println!("Here, my spaces type is {}", type_of(spaces));
+    println!("Amazing!")
 }
